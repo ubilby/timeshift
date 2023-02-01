a057b7f08fb20cd4e995c36cee9984527a42d66d
ubilby <ubilby@gmail.com>
2023-02-01 20:27:11 +0400

    init commit

---
+++ main.py
@@ -0,0 +1,12 @@
+'''
+Application of viewing locacl times of cityes
+
+There are threee puncts menu:
+    view list of cityes
+    add city
+    exit
+
+There is findig city by service abstracpapi.com
+If city will be find name of city will be save to local storage of the app
+'''
+
