a07b7300617757682e24f39567eac1ef935fe93b
ubilby <ubilby@gmail.com>
2023-02-01 22:27:19 +0400

    add viewing time of entering city

--- main.py
+++ main.py
@@ -9,4 +9,45 @@ There are threee puncts menu:
 There is findig city by service abstracpapi.com
 If city will be find name of city will be save to local storage of the app
 '''
+import datetime
+from typing import Dict
+import requests
 
+ABSTRACTAPI_URL: str = (
+    'https://timezone.abstractapi.com/v1/current_time?'
+    'api_key=6585964da71f44a5aa9b37a5232a1c13&location='
+)
+while True:
+    print(
+        '\nEnter command:\n'
+        '1. View list of cityes\n'
+        '2. Add city\n'
+        '3. Exit'
+    )
+    command: str = input(">>> ")
+
+    if command == '1':
+        ...
+
+    elif command == '2':
+        city_name: str = input("Enter city name: ")
+        print(city_name)
+        city_data: Dict[str, str] = (
+            requests.get(ABSTRACTAPI_URL + city_name).json()
+        )
+
+        if not city_data:
+            print(f'There is not city {city_name}')
+
+        else:
+            local_time = (
+                datetime.datetime.now(datetime.timezone.utc)
+                + datetime.timedelta(hours=int(city_data['gmt_offset']))
+            ).strftime('%H:%M')
+            print(f'Current time in {city_name} - {local_time}')
+
+    elif command == '3':
+        break
+
+    else:
+        print(f'Unknown command {command}')
