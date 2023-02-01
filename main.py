20bde18f704a822879f21e7bda3f9752da8a7070
ubilby <ubilby@gmail.com>
2023-02-01 23:28:21 +0400

    done func style

--- main.py
+++ main.py
@@ -13,50 +13,79 @@ import datetime
 from typing import Dict
 import requests
 
-ABSTRACTAPI_URL: str = (
-    'https://timezone.abstractapi.com/v1/current_time?'
-    'api_key=6585964da71f44a5aa9b37a5232a1c13&location='
-)
-cities: Dict[str, int] = dict()
 
-while True:
+def show_menu() -> None:
     print(
-        '\nEnter command:\n'
-        '1. View list of cityes\n'
-        '2. Add city\n'
-        '3. Exit'
+          '\nEnter command:\n'
+          '1. View list of cityes\n'
+          '2. Add city\n'
+          '3. Exit'
     )
-    command: str = input(">>> ")
 
-    if command == '1':
 
-        for city_name in cities:
-            local_time = (
-                datetime.datetime.now(datetime.timezone.utc)
-                + datetime.timedelta(hours=cities[city_name])
-            ).strftime('%H:%M')
-            print(f'{city_name} - {local_time}')
+def show_cities(cities: Dict[str, int]):
+    current_utc_time: datetime.datetime = (
+        datetime.datetime.now(datetime.timezone.utc)
+    )
+    for city_name in sorted(cities):
+        local_time = get_local_time(cities[city_name], current_utc_time)
+        print(f'{city_name} - {local_time}')
 
-    elif command == '2':
-        city_name: str = input("Enter city name: ")
-        print(city_name)
-        city_data: Dict[str, str] = (
-            requests.get(ABSTRACTAPI_URL + city_name).json()
-        )
 
-        if not city_data:
-            print(f'There is not city {city_name}')
+def find_city() -> Dict[str, str]:
+    ABSTRACTAPI_URL: str = (
+        'https://timezone.abstractapi.com/v1/current_time?'
+        'api_key=6585964da71f44a5aa9b37a5232a1c13&location='
+    )
+    
+    city_name: str = input("Enter city name: ")
+    print(city_name)
+    city_data: Dict[str, str] = (
+        requests.get(ABSTRACTAPI_URL + city_name).json()
+    )
+
+    if not city_data:
+        print(f'There is not city {city_name}')
+        return {}
+
+    return {
+        "name": city_name,
+        "gmt_offset": city_data["gmt_offset"]
+    }
+
+
+def get_local_time(
+    gmt_offset: int,
+    base_time: datetime.datetime = datetime.datetime.now(datetime.timezone.utc)
+) -> str:
+
+    return (
+        base_time + datetime.timedelta(hours=gmt_offset)
+    ).strftime('%H:%M')
+
+
+if __name__ == '__main__':
+    cities: Dict[str, int] = dict()
+
+    while True:
+        show_menu()
+        command: str = input(">>> ")
+
+        if command == '1':
+            show_cities(cities)
+
+        elif command == '2':
+            city_data = find_city()
+
+            if not city_data:
+                continue
+
+            cities[city_data['name']] = int(city_data['gmt_offset'])
+            local_time = get_local_time(cities[city_data['name']])
+            print(f'Current time in {city_data["name"]} - {local_time}')
+
+        elif command == '3':
+            break
 
         else:
-            local_time = (
-                datetime.datetime.now(datetime.timezone.utc)
-                + datetime.timedelta(hours=int(city_data['gmt_offset']))
-            ).strftime('%H:%M')
-            print(f'Current time in {city_name} - {local_time}')
-            cities[city_name] = int(city_data['gmt_offset'])
-
-    elif command == '3':
-        break
-
-    else:
-        print(f'Unknown command {command}')
+            print(f'Unknown command {command}')
