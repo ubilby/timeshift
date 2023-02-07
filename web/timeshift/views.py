c48fe800145e7dd39e5b5cb45e7498a32bc52381
ubilby <ubilby@gmail.com>
2023-02-07 23:11:26 +0400

    view time

--- web/timeshift/views.py
+++ web/timeshift/views.py
@@ -5,9 +5,15 @@ from timeshift import timeshift_lib as ts
 
 # Create your views here.
 def index(request):
+    response_data = dict()
+
     if request.method == 'POST':
         city_name = request.POST['city_name']
         api = ts.AbstractAPI()
-        city_data = api.fetch_city_data(city_name)
-        print('>>>', city_data)
-    return render(request, 'timeshift/index.html')
+        response_data = api.fetch_city_data(city_name)
+        if not response_data.get('error', False):
+            response_data['local_time'] = ts.get_local_time(
+                int(response_data['gmt_offset'])
+            )
+
+    return render(request, 'timeshift/index.html', response_data)
